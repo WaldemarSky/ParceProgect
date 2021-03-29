1c180ff627e77d54dfb449d7705f7cc99cc5f0b8
Vladimir <VladimirKamensk92@yandex.ru>
2021-03-29 19:34:25 +0700

    version 5. with update

--- Parse.py
+++ Parse.py
@@ -2,9 +2,19 @@ import requests
 import datetime
 from requests.exceptions import HTTPError
 import os
+import time
+import fake_useragent
 
 page_exchanges = "https://www.cmegroup.com/CmeWS/mvc/Volume/TradeDates?exchange="
 page_oi_profiles = "https://www.cmegroup.com/CmeWS/mvc/Volume/Details/F/{0}/{1}/{2}?tradeDate={1}"
+
+user = fake_useragent.UserAgent().random
+
+header = {
+    'user-agent': user
+}
+
+se = requests.session()
 
 oi_profiles = {
     'chicago_srw_wheat_cbot':       {'number': '323',   'exchange': 'CBOT'},
@@ -44,6 +54,8 @@ oi_profiles = {
     'euro_fx_cme':                  {'number': '58',    'exchange': 'CME'},
     'british_pound_cme':            {'number': '42',    'exchange': 'CME'},
     'canadian_dollar_cme':          {'number': '48',    'exchange': 'CME'},
+    'australian_dollar_cme':        {'number': '37',    'exchange': 'CME'},
+    'new_zealand_dollar_cme':       {'number': '78',    'exchange': 'CME'},
     'japanese_yen_cme':             {'number': '69',    'exchange': 'CME'},
     'swiss_franc_cme':              {'number': '86',    'exchange': 'CME'},
     'russian_ruble_cme':            {'number': '83',    'exchange': 'CME'},
@@ -52,6 +64,7 @@ oi_profiles = {
 }
 
 file_directory = ""
+point_date = datetime.datetime.now().date() - datetime.timedelta(40)
 
 def delete_mark(str, mark):
     #Удаляет из строки ненужный символ
@@ -64,7 +77,7 @@ def delete_mark(str, mark):
 def get_get (page):
     #Получает url и извлекает из него объект json
     try:
-        result = requests.get(page)
+        result = requests.get(page, headers=header)
     except HTTPError as http_err:
         print(f"Ошибка HTTP: {http_err}!")
     except Exception as err:
@@ -73,22 +86,22 @@ def get_get (page):
         print(f"Запрос к странице: {page} \n Успешен!")
         return result.json()
 
-def parse_to_file(instrument):
+def parse_to_file(instrument, date, recording_mode):
     try:
         print(f"Обработка инструмента {instrument}")
         page_status = page_exchanges + oi_profiles[instrument]['exchange']
         status_dict = get_get(page_status)
         if file_directory == "":
             directory_folder = r"{}\{}.txt".format(os.path.dirname(os.path.abspath(__file__)), instrument)
-            file = open(directory_folder, 'w')
+            file = open(directory_folder, recording_mode)
         else:
             directory_folder = r"{}\{}.txt".format(file_directory, instrument)
             folder_path = os.path.dirname(directory_folder)  # Путь к папке с файлом
             if not os.path.exists(folder_path):  # Если пути не существует создаем его
                 os.makedirs(folder_path)
-            file = open(directory_folder, 'w')
+            file = open(directory_folder, recording_mode)
 
-        current_date = datetime.datetime.now().date() - datetime.timedelta(40)
+        current_date = date
         today_date = datetime.datetime.now().date()
 
         while current_date <= today_date:
@@ -128,11 +141,75 @@ def parse_to_file(instrument):
             file.write('\n')
             print(f"Данные по дате {current_date} для инструмента {instrument} успешно добавлены!\n")
             current_date = current_date + datetime.timedelta(1)
+            time.sleep(5)
 
     except Exception as err:
         print(f"Что-пошло не так при обработке инструмента {instrument}: {err}")
     else:
         print(f"Файл {instrument}.txt успешно обработан!\n")
+
+def red_end_line(file, number_line_fr_end = 0):
+    count_n = -1
+    pos = 1
+    while count_n != number_line_fr_end:
+        str = ""
+        ch = ''
+        while True:
+            pos += 1
+            file.seek(-pos, 2)
+            ch = file.read(1)
+            ch = ch.decode('UTF-8')
+            if ch == '\n':
+                break
+            str = ch + str
+        count_n +=1
+    return str
+
+def get_seek_endline(file, number_line_fr_end = 0):
+    count_n = -1
+    pos = 1
+    while count_n != number_line_fr_end:
+        while True:
+            pos += 1
+            file.seek(-pos, 2)
+            ch = file.read(1)
+            ch = ch.decode('UTF-8')
+            if ch == '\n':
+                break
+        count_n += 1
+    return pos
+
+def update():
+    if file_directory == "":
+        directory_folder = r"{}".format(os.path.dirname(os.path.abspath(__file__)))
+    else:
+        directory_folder = r"{}".format(file_directory)
+
+    files = os.listdir(directory_folder)
+    files_list = list(filter(lambda x: x.endswith('.txt'), files))
+    instr_list = []
+    for file in files_list:
+        instr = file.split('.')[0]
+        if instr in oi_profiles.keys():
+            instr_list.append(instr)
+
+    for instrument in instr_list:
+        file_instr = open(f"{instrument}.txt", 'ab+')
+        ln = red_end_line(file_instr)
+        list_line = ln.split('\t')
+        date_str = list_line[0].split('.')
+        date = datetime.date(year=int(date_str[0]), month=int(date_str[1]), day=int(date_str[2]))
+        if (list_line[1] == "PRELIM"):
+            pos = get_seek_endline(file_instr)
+            file_instr.seek(-pos, 2)
+            file_instr.truncate()
+        else:
+            date += datetime.timedelta(1)
+
+        file_instr.close()
+        parse_to_file(instrument, date, 'a+')
+
+
 
 def keep_window_open():
     a = ""
@@ -147,39 +224,43 @@ try:
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
             "Accept-Language": "en"
         }
-    instrument_list = list(oi_profiles.keys())
+        instrument_list = list(oi_profiles.keys())
 
-    my_instrument_list = instrument_list
+        my_instrument_list = instrument_list
 
-    print("Добро пожаловать в программу-парсер Open Interest\n")
-    print("Для выбора копируемых биржевых инструментов введите 1, для ввода директории копирования 2.\nДля старта программы введите go\n")
+        print("Добро пожаловать в программу-парсер Open Interest\n")
+        print("Для выбора копируемых биржевых инструментов введите 1, для ввода директории копирования 2.\nДля старта первоначального копирования введите go, для обновления по инстументам up\n")
 
-    while True:
-        print(f"Текущие настройки:\nКопируемые инструменты:")
-        for instr in my_instrument_list:
-            print(instr)
-        print(f"Текущая директория: {file_directory}. Если директория пустая, то файлы создаются в директории с программой.")
-        a = input(">>>")
-        if a == '1':
-            my_instrument_list = []
-            print("Введите через пробел номера копируемых инстументов:")
-            for i in range(1, len(instrument_list)+ 1):
-                print(f"{i} - {instrument_list[i-1]}")
-            b = input(">>>").split()
-
-            num_list = list(map(int, b))
-            for x in num_list:
-                if x > 0 and x <= len(instrument_list):
-                    my_instrument_list.append(instrument_list[x-1])
-                else:
-                    print(f"Нет инструмента с таким номером: {x}")
-        if a == '2':
-           file_directory = input("Введите необходимую директорию для сохранения файлов:\n>>>")
-        if a == 'go':
-            print("Запуск парсинга...")
+        while True:
+            print(f"Текущие настройки:\nКопируемые инструменты:")
             for instr in my_instrument_list:
-                parse_to_file(instr)
-            break
+                print(instr)
+            print(f"Текущая директория: {file_directory}. Если директория пустая, то файлы создаются в директории с программой.")
+            a = input(">>>")
+            if a == '1':
+                my_instrument_list = []
+                print("Введите через пробел номера копируемых инстументов:")
+                for i in range(1, len(instrument_list)+ 1):
+                    print(f"{i} - {instrument_list[i-1]}")
+                b = input(">>>").split()
+
+                num_list = list(map(int, b))
+                for x in num_list:
+                    if x > 0 and x <= len(instrument_list):
+                        my_instrument_list.append(instrument_list[x-1])
+                    else:
+                        print(f"Нет инструмента с таким номером: {x}")
+            if a == '2':
+               file_directory = input("Введите необходимую директорию для сохранения файлов:\n>>>")
+            if a == 'go':
+                print("Запуск парсинга...")
+                for instr in my_instrument_list:
+                    parse_to_file(instr, point_date, 'w')
+                break
+            if a == 'up':
+                print("Запуск обновления инструментов...")
+                update()
+                break
 
 except Exception as err:
     print(f"Что-пошло не так при работе программы: {err}")
